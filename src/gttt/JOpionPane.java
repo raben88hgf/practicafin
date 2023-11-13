9efc6559a5d6b7d8399b11b0bf863bc8c2827b84
22PROGB24 <22PROGB24@TS-LABS-4.labs.cbtis94.local>
2023-11-13 08:14:16 -0600

    ejemplo

---
+++ src/gttt/JOpionPane.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package gttt;
+
+import javax.swing.JRootPane;
+
+/**
+ *
+ * @author 22PROGB24
+ */
+class JOpionPane {
+
+    static void showMessageDialog(JRootPane rootPane, String error_seleccione_el_mes) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
